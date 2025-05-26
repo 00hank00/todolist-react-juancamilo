1c93374e056a509f30099871635e1a3d88a07feb
juan camilo <oohankoo@hotmail.com>
2025-05-26 19:01:55 +0000

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import './App.css';
 import { Task } from './Task';
 import todoService from './todo-service';
 
-//const TODO_LIST = [
-  //{ label: 'Ir a la playa' },
-  //{ label: 'Cheking en el hotel' },
-  //{ label: 'Reservar en restaurante' },
-//];
 
 const INPUT_DEFAULT_VALUE = '';
 
@@ -23,17 +18,55 @@ function App() {
     loadInitialValues().then();
   }, []);
 
-  const handleAddTask = () => {
-    if (input) {
-      setTodoList([...todoList, { label: input }]);
-      setInput(INPUT_DEFAULT_VALUE);
+const handleAddTask = async () => {
+  if (input) {
+   
+    setTodoList([...todoList, { label: input }]);
+    setInput(INPUT_DEFAULT_VALUE);
+ 
+    const username = "juanca"; 
+    
+    try {
+      await fetch(`https://playground.4geeks.com/todo/todos/${username}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          label: input,
+          is_done: false
+        })
+      });
+    } catch (error) {
+      console.error("Error al agregar la tarea al servidor:", error);
     }
-  };
+  }
+};
+
 
   const handleDelete = (id) => {
     const filteredTodoList = todoList.filter((task, index) => index !== id)
     setTodoList(filteredTodoList)
   };
+
+const handleClearAll = async () => {
+  try {
+    await Promise.all(
+      todoList.map(todo =>
+        fetch(`https://playground.4geeks.com/todo/todos/${todo.id}`, {
+          method: 'DELETE'
+        })
+      )
+    );
+
+    setTodoList([]);
+    console.log("Todas las tareas han sido eliminadas.");
+  } catch (error) {
+    console.error("Error al eliminar todas las tareas:", error);
+  }
+};
+
+
 
   return (
     <>
@@ -50,6 +83,8 @@ function App() {
           onChange={(e) => setInput(e.target.value)}
         />
         <button className='todolist-Boton' onClick={handleAddTask}>Add</button>
+        <button className='clearAll-boton' onClick={handleClearAll}>Clear All</button>
+
         </div>
         <ul>
           {todoList.map((task, index) => (
